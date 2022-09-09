000e187b4e8d0f7d8d91d421e45078454bc3506b
SudhakarBadigar <sudhakar.badigar87@gmail.com>
2022-09-09 15:13:37 +0530

    still to complete

--- jsNumberMethod/mathmethods.js
+++ jsNumberMethod/mathmethods.js
@@ -1,7 +1,18 @@
 var a = 10
 var b = Math.min(10,3,4,50,24);
 console.log(b);
-console.log(Math.ceil(10.25));
+console.log(Math.ceil(10.00000000000001));
 console.log(Math.floor(2.99));
 console.log(Math.round(20.87));
-console.log(Math.trunc(9.0000000000000000000000000000000000000000000000000000000000000000000000001));
+console.log(Math.trunc(9.9999999999999999999999999900000000000000000000001));
+console.log(Math.max(10,20,5,0.4,-44,1000));
+console.log(Math.sqrt(64));
+console.log(Math.cbrt(125));
+console.log(Math.pow(3,3));
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.abs(-5.000001));
+console.log(Math.abs("hello"));
+console.log(Math.abs(0.1+ 0.2));
+console.log(0.1 +0.2);
+console.log(Math.PI);
